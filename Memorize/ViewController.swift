c06899772678df22b830f0d21dea69f12ea2862e
Danny <dantes817@gmail.com>
2020-05-02 18:46:49 +0200

    Initial Commit

---
+++ Memorize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorize
+//
+//  Created by Danny on 02/05/2020.
+//  Copyright © 2020 Elmob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
