8d16673da007fc82609c33b5f9d109b29253be85
Danny <dantes817@gmail.com>
2020-05-03 16:11:53 +0200

    Initial commit/ decoded JSON to Table struct. on Ui

---
+++ Memorize/MemoryItem.swift
@@ -0,0 +1,15 @@
+//
+//  MemoryItem.swift
+//  Memorize
+//
+//  Created by Danny on 03/05/2020.
+//  Copyright © 2020 Elmob. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryItem: Codable {
+    var title: String,
+        text: String
+    
+}
